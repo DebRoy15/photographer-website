915a0730d39c057e1c153ca6856e5f0f4e12fd30
DebRoy15 <66943034+DebRoy15@users.noreply.github.com>
2020-12-25 12:28:17 +0530

    services section created

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,54 @@
+import React from "react";
+//import icon
+import clock from "../img/clock.svg";
+import diaph from "../img/diaphragm.svg";
+import money from "../img/money.svg";
+import teamwork from "../img/teamwork.svg";
+import home2 from "../img/home2.png";
+
+const ServicesSection = () => {
+  return (
+    <div className="services">
+      <div className="description">
+        <h2>
+          High <span>quality</span> services
+        </h2>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={clock} alt="" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={teamwork} alt="" />
+              <h3>Team Work</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={diaph} alt="" />
+              <h3>Diaphragm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={money} alt="" />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+      <div className="image">
+        <img src={home2} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AboutUsSection from "../components/AboutUsSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
   return (
-    <div>
+    <>
       <AboutUsSection />
-    </div>
+      <ServicesSection />
+    </>
   );
 };
 
